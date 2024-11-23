5c8996a9f29dae1299b0fa15f0847ee0fb77df1c
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-23 18:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function stringChop(str, size) {
-  // your code here
+
+	let ansarr=[];
+	let count=0;
+	let word="";
+
+	for(let i=0;i<str.length;i++)
+		{
+			count++;
+			word+=str.charAt(i);
+			if(count===size)
+			{
+				ansarr.push(word);
+				count=0;
+				word="";
+			}
+		}
+
+	if(count<size)
+	{
+		ansarr.push(word);
+	}
+
+	return ansarr;
 }
 
 // Do not change the code below
